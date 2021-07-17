d7534c8a658678eb0580a816de07741a4b2fb193
yyn <543058483@qq.com>
2021-07-17 22:26:01 +0800

    optimize readme

--- oprf/psi_bin/recv.sh
+++ oprf/psi_bin/recv.sh
@@ -1,2 +1,3 @@
 #     0:send,1:recv  
-./psi -r 1 -w 60 -h 10 -ss 5000000 -rs 5000000 -sd 22 -in ../../../500w_2.txt -out ../../../500w_2_save.txt
+./psi -r 1 -w 60 -h 10 -ss 50000000 -rs 50000000 -sd 22 
+#-in ../../../500w_2.txt -out ../../../500w_2_save.txt
